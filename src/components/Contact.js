b4b936f5336057e978e55be8cf8e44d32a369c87
Albenoo <albeniskerqeli5@gmail.com>
2021-08-22 22:50:38 +0200

    change icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/contact.scss';
+import {FaLinkedin , FaGithub , FaTwitter , FaInstagram , FaFacebook } from 'react-icons/fa'
 const Contact = () => {
     return (
         <section className="contact">
-            <h1 className="">I can help :)</h1>
+            <h1 className="text">I can help :)</h1>
         
 <p>I’m currently available for a office or freelance job<br/>
 If you have a project that you want to get started or any job  position related to my skills,<br/> you can contact me on  social media accounts</p>
@@ -12,12 +13,11 @@ If you have a project that you want to get started or any job  position related 
 <h4>albeniskerqeli5@gmail.com</h4>
 </div> */}
 <div className="contact__icons">
-<i className="bi bi-linkedin"></i>
-<i className="bi bi-github"></i>
-<i className="bi bi-twitter"></i>
-<i className="bi bi-instagram"></i>
-
-<i className="bi bi-facebook"></i>
+<i className="contact__icon"><FaLinkedin/></i>
+<i className="contact__icon"><FaGithub/></i>
+<i className="contact__icon"><FaTwitter/></i>
+<i className="contact__icon"><FaInstagram/></i>
+<i className="contact__icon"><FaFacebook/></i>
 
 </div>
         </section>
--- src/data/data.js
+++ src/data/data.js
@@ -13,7 +13,6 @@ export const projects  = [
         image:'https://res.cloudinary.com/albenis/image/upload/v1629466504/covid_pyfhzg.webp',
         description:'a Covid 21 Tracker App, which tracks real-time covid data worldwide',
         technologies:'Javascript, HTML, CSS, API',
-
         livedemo:'https://albenis-kerqeli.github.io/Covid-19-Tracker-App/',
         sourcecode:'https://github.com/Albenis-Kerqeli/Covid-19-Tracker-App',
     },
@@ -22,48 +21,14 @@ export const projects  = [
         image:'https://res.cloudinary.com/albenis/image/upload/v1629583391/ecommercecomwebp_in5eih.webp',
         description:'A website development and design  for a food ordering application,a figma design converted to react website',
         technologies:'React, HTML, CSS',
-
-        livedemo:'https://albenis-kerqeli.github.io/Covid-19-Tracker-App/',
-        sourcecode:'https://github.com/Albenis-Kerqeli/Covid-19-Tracker-App',
-    },
+        livedemo:'https://react-design-d6e9f.web.app/',
+        },
+        
       {
         name:"Todo App ",
         image:'https://res.cloudinary.com/albenis/image/upload/v1629465888/todowebp_dtr9jh.webp ',
         description:'A simple todo App built with React and Firebase, that you canadd,edit and delete your tasks in a simple way',
         technologies:'React, HTML, CSS, Firebase',
-
-        livedemo:'https://albenis-kerqeli.github.io/Covid-19-Tracker-App/',
-        sourcecode:'https://github.com/Albenis-Kerqeli/Covid-19-Tracker-App',
+        livedemo:'https://todo-app-2021-54205.web.app/',
     }
 ]
-// JSON DATA 
-// export const projects  = [
-//     {
-//         name:"Ecommerce Web App",
-//         image:'https://res.cloudinary.com/albenis/image/upload/v1629328183/ecommerce_pdkq2i.jpg',
-//         description:'An Ecommerce Wb App built with ReactJs among wih his libraries, also provider Paypal for products checkout',
-//         technologies:'React/HTML/CSS/Paypal',
-//         livedemo:'https://reactjs-commerce.netlify.app/',
-//         sourcecode:'https://github.com/Albenis-Kerqeli/Reactjs-Ecommerce-App',
-//     } , 
-
-//     {
-//         name:"Covid 19 Tracker App",
-//         image:'http://res.cloudinary.com/albenis/image/upload/v1629328226/covid_hbpruf.jpg',
-//         description:'a Covid 21 Tracker App, which tracks real-time covid data worldwide',
-//         technologies:'Javascript/HTML/CSS/API',
-
-//         livedemo:'https://albenis-kerqeli.github.io/Covid-19-Tracker-App/',
-//         sourcecode:'https://github.com/Albenis-Kerqeli/Covid-19-Tracker-App',
-//     },
-//     {
-//         name:"Pizzery Landing Website",
-//         image:'http://res.cloudinary.com/albenis/image/upload/v1629328226/covid_hbpruf.jpg',
-//         description:'a Covid 21 Tracker App, which tracks real-time covid data worldwide',
-//         technologies:'Javascript/HTML/CSS/API',
-
-//         livedemo:'https://albenis-kerqeli.github.io/Covid-19-Tracker-App/',
-//         sourcecode:'https://github.com/Albenis-Kerqeli/Covid-19-Tracker-App',
-//     }
-// ]
-
