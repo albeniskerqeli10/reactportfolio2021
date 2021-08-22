826306eb32df319c47f1a60f54fd7120e5991d78
Albenoo <albeniskerqeli5@gmail.com>
2021-08-23 01:27:34 +0200

    suspense

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React , {Suspense} from 'react'
 import Skills from '../components/Skills'
 import TwoColumn from '../components/TwoColumn'
 
 const About = React.memo(() => {
     return (
+        <Suspense fallback={null}>
         <div>
             <TwoColumn/>
             <Skills/>
             
         </div>
+        </Suspense>
     )
 }
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import Hero from '../components/Hero.js';
 import Projects from '../components/Projects.js';
 const Home = React.memo(() => {
     return (
-<>
+<Suspense fallback={null}>
 <Hero/>
   <Projects/>
-</>
+</Suspense>
     )
 }
 )
